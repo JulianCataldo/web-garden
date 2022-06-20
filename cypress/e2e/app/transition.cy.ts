76562d5c4034da77f5410a53ef4e1d6f3d66b1c4
Julian Cataldo <contact@juliancataldo.com>
2022-06-20 09:19:09 +0200

    test: app transition component

---
+++ cypress/e2e/app/transition.cy.ts
@@ -0,0 +1,27 @@
+describe('App — <Transition>', () => {
+  it('Open demo', () => {
+    cy.visit('http://localhost:2424/');
+  });
+
+  it('Click link other page', () => {
+    cy.wait(1200);
+    cy.get('[href="/page-a"]').click();
+  });
+
+  it('Overlay class is toggled', () => {
+    cy.wait(50);
+    cy.url().should('eq', 'http://localhost:2424/');
+    cy.get('#app-transition-overlay.is-visible');
+  });
+  it('Click link home', () => {
+    cy.wait(2300);
+
+    cy.get('#app-transition-overlay:not(.is-visible)');
+    cy.get('[href="/"]').click();
+  });
+  it('Overlay class is toggled', () => {
+    cy.wait(50);
+    cy.url().should('eq', 'http://localhost:2424/page-a');
+    cy.get('#app-transition-overlay.is-visible');
+  });
+});
