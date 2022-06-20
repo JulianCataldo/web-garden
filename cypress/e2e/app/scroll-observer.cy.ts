ae6eb1ebce90da7e3c5f6ec04121f603b01c8f8e
Julian Cataldo <contact@juliancataldo.com>
2022-06-20 06:31:34 +0200

    test: add scroll detection

--- cypress/e2e/app/scroll-observer.cy.ts
+++ cypress/e2e/app/scroll-observer.cy.ts
@@ -5,6 +5,7 @@ describe('App — <ScrollObserver>', () => {
 
   it('Base state', () => {
     cy.get('body')
+      .should('have.attr', 'data-has-scroll', 'true')
       .should('have.attr', 'data-is-scrolling-up', 'false')
       .should('have.attr', 'data-is-bottom', 'false')
       .should('have.attr', 'data-is-top', 'true');
