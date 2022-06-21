32650bc88140b851d76b09234372af0ff5a5459e
Julian Cataldo <contact@juliancataldo.com>
2022-06-21 09:02:19 +0200

    test: fix cross origin bug

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  // This fix a weird bug when navigating to home link
+  chromeWebSecurity: false,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
