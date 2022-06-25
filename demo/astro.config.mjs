2dcf545ff1c8b38d5e3027e34254d9111dcb3c24
Julian Cataldo <contact@juliancataldo.com>
2022-06-25 20:22:46 +0200

    feat: color mode integration in demo

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -3,4 +3,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   server: { port: 2424, host: false },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@julian_cataldo/astro-color-mode/use-mixins.scss" as *;
+            // @use "@julian_cataldo/astro-breakpoints/use-breakpoints.scss" as *;`,
+        },
+      },
+    },
+  },
 });
