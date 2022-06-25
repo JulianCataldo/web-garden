0357745169eb2eba01a4eebbabcbf268ffe94331
Julian Cataldo <contact@juliancataldo.com>
2022-06-25 17:25:41 +0200

    test: wait a bit between hovers

--- cypress/e2e/app/prefetch.cy.ts
+++ cypress/e2e/app/prefetch.cy.ts
@@ -5,10 +5,14 @@ describe('App — <Prefetch>', () => {
 
   it('Check for 3 on 4 Link tags', () => {
     cy.get('.link-card:nth-of-type(1) a').realHover();
+    cy.wait(50);
     cy.get('.link-card:nth-of-type(2) a').realHover();
+    cy.wait(50);
     cy.get('.link-card:nth-of-type(3) a').realHover();
+    cy.wait(50);
     cy.get('.link-card:nth-of-type(4) a').realHover();
 
+    cy.wait(50);
     cy.get('head link').should('have.length', 3);
   });
 });
