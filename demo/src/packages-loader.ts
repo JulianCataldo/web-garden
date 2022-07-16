b8dda4c1e9d86f060d8968b7c3443f2b159b0abb
Julian Cataldo <contact@juliancataldo.com>
2022-07-16 08:03:45 +0200

    feat: add package loader utility

---
+++ demo/src/packages-loader.ts
@@ -0,0 +1,119 @@
+import type { MarkdownInstance } from 'astro';
+import type { MarkdownHeader } from '@astrojs/markdown-remark';
+import type { AstroComponentFactory } from 'astro/dist/types/runtime/server';
+import fs from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import glob from 'glob-promise';
+
+interface PJson {
+  name: string;
+  description: string;
+  repository: {
+    url: string;
+    directory: string;
+  };
+}
+interface Readme {
+  mainTitle: MarkdownHeader;
+  headers: MarkdownHeader[] | null;
+  Content: AstroComponentFactory | null;
+}
+export interface Package {
+  pJson: PJson;
+  readme: Readme;
+  typescriptProps: string | null;
+  video: string | null;
+  shortname: string | null;
+  hasDemo: boolean;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export async function structureAllPackages(
+  pJsons: PJson[],
+  readmes: MarkdownInstance<unknown>[],
+  anchorMode = true,
+) {
+  const packages: Package[] = [];
+
+  pJsons.forEach((pJson: PJson) => {
+    packages.push({
+      pJson: {
+        name: pJson.name,
+        description: pJson.description,
+        repository: pJson.repository,
+      },
+      readme: {
+        mainTitle: { text: '', slug: '', depth: 0 },
+        headers: null,
+        Content: null,
+      },
+      typescriptProps: null,
+      video: null,
+      shortname: null,
+      hasDemo: false,
+    });
+    console.log(pJson.name);
+  });
+
+  await Promise.all(
+    readmes.map(async (readme, index) => {
+      const headers = await readme.getHeaders();
+      // console.log(readme.file);
+
+      packages[index].readme = {
+        mainTitle: {
+          slug: anchorMode
+            ? headers[0].slug
+            : packages[index].pJson.name.replace('@julian_cataldo/', ''),
+          text: headers[0].text,
+          depth: 0,
+        },
+        headers,
+        Content: readme.Content,
+      };
+
+      // ———————————————————————————————————————————————————————————————————————
+
+      const shortname = packages[index].pJson.name.replace(
+        '@julian_cataldo/',
+        '',
+      );
+      packages[index].shortname = shortname;
+
+      // ———————————————————————————————————————————————————————————————————————
+
+      const dir = packages[index].pJson.repository.directory;
+      packages[index].typescriptProps = await fs
+        .readFile(`./content/packages/${dir}/Props.ts`, 'utf8')
+        .catch((e) => {
+          console.log(e);
+          return null;
+        });
+
+      // ———————————————————————————————————————————————————————————————————————
+
+      const videoName = packages[index].pJson.name
+        .replace('@julian_cataldo/', '')
+        .substring(6);
+
+      /* In Public folder */
+      const videoPath = `/assets/videos/tests/*-${videoName}.cy.ts.mp4`;
+      const video = await (await glob(`./public/${videoPath}`)).pop();
+      console.log({ videoPath, video, videoName });
+      if (video) {
+        packages[index].video = video.replace('./public', '');
+      }
+
+      // ———————————————————————————————————————————————————————————————————————
+
+      const demoPath = `./src/components/Demo/${shortname}.astro`;
+      const hasDemo = existsSync(demoPath);
+      // console.log({ demoPath });
+      if (hasDemo) {
+        packages[index].hasDemo = true;
+      }
+    }),
+  );
+
+  return packages;
+}
