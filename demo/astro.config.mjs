2c2a508a3a9608b5dacb7fb0fa928dda85210292
Julian Cataldo <contact@juliancataldo.com>
2022-07-16 08:00:57 +0200

    chore: update astro base config

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,40 +1,72 @@
 import { defineConfig } from 'astro/config';
-
-const breakpoints = {
-  xs: '320px',
-  sm: '576px',
-  md: '768px',
-  lg: '992px',
-  xl: '1200px',
-  xxl: '1840px',
-};
+import { astroImageTools } from 'astro-imagetools';
+import sitemap from '@astrojs/sitemap';
+// import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 2424, host: false },
+  server: {
+    port: 2424,
+    host: false,
+  },
+  // base: '/astro',
 
-  site: 'https://juliancataldo.github.io',
-  base: '/astro',
+  integrations: [
+    sitemap(),
+    astroImageTools,
+    // react(),
+  ],
 
   vite: {
+    /* astro-icon */
+    ssr: {
+      external: ['svgo'],
+    },
+
+    plugins: [],
+
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: `
-            @use "@julian_cataldo/astro-breakpoints/use-breakpoints.scss" as * with (
-              $breakpoints: (
-                "xs": ${breakpoints.xs},
-                "sm": ${breakpoints.sm},
-                "md": ${breakpoints.md},
-                "lg": ${breakpoints.lg},
-                "xl": ${breakpoints.xl},
-                "xxl": ${breakpoints.xxl},
-              )
-            );
-            @use "@julian_cataldo/astro-color-mode/use-color-mode.scss" as *;
-            `,
+            @use "sass:color";
+            @use "./src/themes" as *;
+          `,
         },
       },
     },
   },
+
+  // markdown: {
+  //   remarkPlugins: [ ],
+  //   rehypePlugins: [],
+  // },
+
+  experimental: { integrations: true },
 });
+
+// const breakpoints = {
+//   xs: '320px',
+//   sm: '576px',
+//   md: '768px',
+//   lg: '992px',
+//   xl: '1200px',
+//   xxl: '1840px',
+// };
+// Using `gather-content.sh` for now
+// server: {
+//   fs: {
+//     allow: ['..'],
+//   },
+// },
+// @use "@julian_cataldo/astro-breakpoints/use-breakpoints.scss" as * with (
+//   $breakpoints: (
+//     "xs": ${breakpoints.xs},
+//     "sm": ${breakpoints.sm},
+//     "md": ${breakpoints.md},
+//     "lg": ${breakpoints.lg},
+//     "xl": ${breakpoints.xl},
+//     "xxl": ${breakpoints.xxl},
+//   )
+// );
+// @use "@julian_cataldo/astro-color-mode/use-color-mode.scss" as *;
