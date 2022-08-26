afba8472c4b2b491fc0db6e3da65fa558297851d
Julian Cataldo <contact@juliancataldo.com>
2022-08-26 05:53:17 +0200

    feat: add prettier base preset

---
+++ configs/prettier-base.cjs
@@ -0,0 +1,10 @@
+/** @type {import("@types/prettier").Options} */
+
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
