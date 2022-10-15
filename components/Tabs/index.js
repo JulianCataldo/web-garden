e82d1ff69edc0f2e7c18f6741162c9724359eb5a
Julian Cataldo <contact@juliancataldo.com>
2022-10-15 16:25:55 +0200

    refactor: use crypto instead of nanoid

--- components/Tabs/index.js
+++
@@ -1,3 +0,0 @@
-import Tabs from './Tabs.astro';
-
-export { Tabs };
--- components/Tabs/index.ts
+++ components/Tabs/index.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-import TerminalPlayer from './TerminalPlayer.astro';
+import Tabs from './Tabs.astro';
 
-export { TerminalPlayer };
+export { Tabs };
