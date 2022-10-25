1d057469121529bf46cc10e30752e1125990bf09
Julian Cataldo <contact@juliancataldo.com>
2022-10-25 15:25:26 +0200

    style: props. spacing

--- app/Tooltips/Props.ts
+++ app/Tooltips/Props.ts
@@ -13,22 +13,27 @@ export interface Props extends Partial<TippyProps> {
    * BEWARE: You should trust your sources
    * */
   allowHTML?: boolean;
+
   /**
    * **Default**: `'default'`
    * */
   theme?: string;
+
   /**
    * **Default**: `'none'`
    * */
   maxWidth?: string;
+
   /**
    * **Default**: `[15, 150]`
    * */
   delay?: [number, number];
+
   /**
    * **Default**: `'auto'`
    * */
   placement?: Placement;
+
   /**
    * **Default**: `true`
    * */
