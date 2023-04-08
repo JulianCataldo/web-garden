1cffc5087e9d67b8282521060bf54e5fe10df850
Julian Cataldo <contact@juliancataldo.com>
2023-04-09 01:48:59 +0200

    feat: add prefetching marker

--- components/Link/Props.ts
+++ components/Link/Props.ts
@@ -1,10 +1,17 @@
 /// <reference types="astro/astro-jsx" />
 
 export interface Props extends astroHTML.JSX.AnchorHTMLAttributes {
-  /**
-   * Open external link in a new tab/window.
-   *
-   * **Default**: `true`
-   * */
-  externalInNewTab?: boolean;
+	/**
+	 * Open external link in a new tab/window.
+	 *
+	 * **Default**: `true`
+	 * */
+	externalInNewTab?: boolean;
+
+	/**
+	 * Prefetching marker (`rel=prefetch`)
+	 *
+	 * **Default**: `true`
+	 * */
+	prefetch?: boolean;
 }
