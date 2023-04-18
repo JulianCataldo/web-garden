3c5452b5555ba4ad795507cbb66a011a1fba638a
Julian Cataldo <contact@juliancataldo.com>
2023-04-18 14:20:39 +0200

    feat: add config examples

---
+++ configs/.prettierrc-example.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Options} */
+
+module.exports = {
+  /**
+   * Reference:
+   *
+   * https://github.com/JulianCataldo/web-garden/blob/develop/configs/prettier-astro.cjs
+   *
+   * */
+  ...require('webdev-configs/prettier-astro.cjs'),
+};
